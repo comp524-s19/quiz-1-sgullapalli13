82b1a9922a1b3cca5d454c9cc3381d8a3c9c59c8
Shreya Gullapalli <sgullapalli13@gmail.com>
2019-01-31 18:02:47 +0000

    ff

--- main.hs
+++ main.hs
@@ -1,2 +1,4 @@
-finalGrade:: [a] -> [b] -> a
-finalGrade final | do ((x*y)`div`y) where x<-frst(head(list)), y<-snd(head(list), drop(head(list)) where list <- zip[a][b] 
+finalGrade :: [a]->[b] -> Int
+finalGrade final | let list = zip[a][b], let final = 0
+	do final = ((x*y)`div`y)+final drop(head(list))
+	where x<-frst(head(list)), y<-snd(head(list)  
